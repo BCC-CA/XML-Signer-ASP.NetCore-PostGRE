c641b87d2f84872a2c66cd5b5edb6ab2cbebfcba
S. M. Abrar Jahin <abrarjahin@live.com>
2020-02-03 23:20:39 +0600

    DB updated for foreign key

--- Data/Migrations/20200203171340_XmlFileUpdate.Designer.cs
+++ Data/Migrations/20200203171340_XmlFileUpdate.Designer.cs
@@ -2,14 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using XmlSigner.Data;
 
 namespace XmlSigner.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200203171340_XmlFileUpdate")]
+    partial class XmlFileUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20200203171340_XmlFileUpdate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XmlSigner.Data.Migrations
+{
+    public partial class XmlFileUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_XmlFiles_SignerId",
+                table: "XmlFiles",
+                column: "SignerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_XmlFiles_AspNetUsers_SignerId",
+                table: "XmlFiles",
+                column: "SignerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_XmlFiles_AspNetUsers_SignerId",
+                table: "XmlFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_XmlFiles_SignerId",
+                table: "XmlFiles");
+        }
+    }
+}
