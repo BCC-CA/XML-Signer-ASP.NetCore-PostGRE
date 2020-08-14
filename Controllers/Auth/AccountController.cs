5067790149af6a741dcb824eeecacb01855dccc2
Abrar Jahin <abrarjahin@live.com>
2020-08-14 23:28:40 +0600

    Mail Service Config

--- Controllers/Auth/AccountController.cs
+++ Controllers/Auth/AccountController.cs
@@ -8,12 +8,12 @@ namespace XmlSigner.Controllers.Auth
 {
     public class AccountController : Controller
     {
-        private readonly UserManager<User> userManager;
+        private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> signInManager;
         public AccountController(UserManager<User> userManager,
             SignInManager<User> signInManager)
         {
-            this.userManager = userManager;
+            this._userManager = userManager;
             this.signInManager = signInManager;
         }
         public IActionResult Regiter()
@@ -36,9 +36,25 @@ namespace XmlSigner.Controllers.Auth
                     UserName = model.Email,
                     Email = model.Email,
                 };
-                var result = await userManager.CreateAsync(user, model.Password);
+                var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var confirmationLink = Url.Action(nameof(ConfirmEmail), "Account", new { token, email = user.Email }, Request.Scheme);
+
+                    /*
+                                        // For more information on how to enable account confirmation and password reset please   
+                                        //visit https://go.microsoft.com/fwlink/?LinkID=532713  
+                                        // Send an email with this link  
+                                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                                        var callbackUrl = Url.Action(nameof(ConfirmEmail), "Account",
+                                        new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                                        await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
+                                           $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
+
+                                        TempData["Message"] = "Confirmation Email has been send to your email. Please check email.";
+                                        TempData["MessageValue"] = "1";
+                    */
                     await signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("Index", "Home");
                 }
---
+++ Services/EmailSender.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace XmlSigner.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private IConfiguration _configuration;
+        public EmailSender(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+        /*
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute(Options.SendGridKey, subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("Joe@contoso.com", Options.SendGridUser),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+        */
+
+        //https://code-maze.com/email-confirmation-aspnet-core-identity/
+        //https://ethereal.email/create
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(_configuration["SMTP:host"])    //_configuration["SMTP:port"]
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_configuration["SMTP:user"], _configuration["SMTP:pass"])
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_configuration["SMTP:user"])
+            };
+            mailMessage.To.Add(email);
+            mailMessage.Subject = subject;
+            mailMessage.Body = htmlMessage;
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
